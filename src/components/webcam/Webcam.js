47ac056caae9f7d6915d666c2afbd9127821c9d6
Jonas Stiernström <stiernstrom_3@hotmail.com>
2019-04-09 15:56:30 +0200

    Created webcam component

---
+++ src/components/webcam/Webcam.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Webcam extends Component {
+  componentDidMount() {
+    const video = document.getElementById("webcam");
+
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "user",
+          width: 600,
+          height: 500
+        }
+      })
+      .then(stream => {
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+          video.play();
+        };
+      });
+  }
+
+  render() {
+    return <video id="webcam" />;
+  }
+}
+
+export default Webcam;
